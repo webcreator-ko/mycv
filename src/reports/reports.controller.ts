7b8cd4b81d7bd298d51815d20747dedbe65fd5ea
Maeda <webcreator.ko@gmail.com>
2025-06-30 11:17:47 +0900

    セクション17: Query Builders with TypeORM

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -17,6 +17,7 @@ export class ReportsController {
   @Get()
   getEstimate(@Query() query: GetEstimateDto) {
     console.log(query)
+    return this.reportsService.crateEstimate(query)
   }
 
   @Post()
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dto/create-report.dto';
 import { User } from "src/users/user.entity";
+import { GetEstimateDto } from "./dto/get-estimate.dto";
 
 @Injectable()
 export class ReportsService {
@@ -16,6 +17,21 @@ export class ReportsService {
         const report = this.repo.create(reportDto);
         report.user = user;
         return this.repo.save(report)
+    }
+
+    crateEstimate(estimateDto: GetEstimateDto) {
+        return this.repo.createQueryBuilder().select('AVG(price)', 'price')
+                .where('make = :make', {make: estimateDto.make})
+                .andWhere('model = :model', {model: estimateDto.model})
+                .andWhere('lng = :lng BETWEEN -5 AND 5', {lng: estimateDto.lng})
+                .andWhere('lat = :lat BETWEEN -5 AND 5', {lat: estimateDto.lat})
+                .andWhere('year = :year BETWEEN -3 AND 3', {year: estimateDto.year})
+                .andWhere('approved IS TRUE')
+                .orderBy('ABS(mileage - :mileage)', 'DESC')
+                // orderByを使用した場合、2つ目の引数は mileage を使用できないので setParameters で渡す
+                .setParameters({milage: estimateDto.mileage})
+                .limit(3)
+                .getRawMany()
     }
 
     async  changeApproval(id: string, approved: boolean) {
